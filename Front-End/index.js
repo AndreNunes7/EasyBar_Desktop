f15aaca06015bcbe1d80ec20c1ae02667d0bb326
peterson.moraes <peterson.moraes@PC-SUPORTE>
2024-10-22 12:08:15 -0300

    refactoring the project structure

---
+++ Front-End/index.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require ("electron");
+
+let mainWindow;
+
+app.on("ready", ()=>{
+
+    mainWindow = new BrowserWindow({
+
+        width:700,
+        height:400
+
+
+
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+})
