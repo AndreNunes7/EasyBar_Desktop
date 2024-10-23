8eecb3717d592f9ccb51413b421c386f7fe1d627
peterson.moraes <peterson.moraes@PC-SUPORTE>
2024-10-23 13:38:56 -0300

    remove menu hamburguer

--- Front-End/index.js
+++ Front-End/index.js
@@ -12,26 +12,14 @@ app.on("ready", () => {
         autoHideMenuBar: true,
         webPreferences: {
             contextIsolation: true,
-            preload: path.join(__dirname, 'js', 'render.js') // Altere o caminho para o preload
+            preload: path.join(__dirname, 'js', 'render.js')
         }
     });
 
     mainWindow.loadFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Função para abrir a janela "Sobre"
-const openAboutWindow = () => {
-    const aboutWindow = new BrowserWindow({
-        width: 360,
-        height: 220,
-        icon: path.join(__dirname, 'img', 'fast-food.png'),
-        autoHideMenuBar: true,
-        resizable: false,
-    });
-    aboutWindow.loadFile(path.join(__dirname, 'views', 'about.html'));
-};
 
-ipcMain.on('open-about-window', openAboutWindow);
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) mainWindow();
