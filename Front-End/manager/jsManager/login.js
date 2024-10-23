b8faaf054d8e4c74541ceee64aa54e6794542d48
peterson.moraes <peterson.moraes@PC-SUPORTE>
2024-10-23 16:39:18 -0300

    update in structure

--- Front-End/manager/jsManager/login.js
+++ Front-End/manager/jsManager/login.js
@@ -1,12 +1,12 @@
 function login() {
-    event.preventDefault(); // Previne o comportamento padrão do formulário
+    event.preventDefault(); 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Aqui você pode adicionar sua lógica de autenticação
-    if (username === 'admin' && password === '123') { // Exemplo simples
+    
+    if (username === 'admin' && password === '123') { 
         alert('Login bem-sucedido!');
-        // Redirecionar para a página de gerenciamento
+        // Redirecionar
         window.location.href = 'admin.html';
     } else {
         alert('Usuário ou senha inválidos.');
